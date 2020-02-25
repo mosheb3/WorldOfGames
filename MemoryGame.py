ad2173d44a1d660b494a74326d1e1081d30f0312
mosheb <mosheb@creditguard.co.il>
2020-02-25 17:50:59 +0200

    create MemoryGame

--- MemoryGame.py
+++ MemoryGame.py
@@ -1,4 +1,19 @@
 ## Moshe Barazani
 ## Date: 25-02-2020
 
+def generate_sequence(difficulty):
+    max_num = 101
+    mlist = random.sample(range(max_num), difficulty)
+    print(mlist)
 
+
+def get_list_from_user():
+    return
+
+
+def is_list_equal():
+    return
+
+
+def play():
+    return
