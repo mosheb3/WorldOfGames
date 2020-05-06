9dfa319ea896e9d6ed6389497b14f443d545c9cf
mosheb3 <mosheb3@gmail.com>
2020-05-06 17:16:47 +0300

    changing webServer.py

--- webServer.py
+++ webServer.py
@@ -7,24 +7,18 @@ import json
 
 app = Flask(__name__)
 
-@app.route('/')
-@app.route('/index')
-@app.route('/clean')
+@app.route('/', methods=['GET', 'POST'])
+@app.route('/index', methods=['GET', 'POST'])
+@app.route('/clean', methods=['GET', 'POST'])
 
 def index():
     fr = open("data/data.json", "r")
     posts_json_a = fr.readlines()
     fr.close()
 
-    #print(posts_json_a)
-
     posts_json_b = "["+(json.dumps(posts_json_a)[2:-2])+"]"
-
     user = {'username': 'WorldOfGames'}
 
-    #posts_json = [{'gamername': 'moshe1', 'game_name': 'guess', 'score': 123123},
-    #              {'gamername': 'moshe2', 'game_name': 'guess', 'score': 12341234},
-    #              {'gamername': 'moshe3', 'game_name': 'guess', 'score': 1234512345}]
     return render_template('score.html', title='WorldOfGames', user=user, posts=eval(posts_json_b))
 
 def clean():
@@ -33,4 +27,5 @@ def clean():
     fw.close()
     return render_template('clean.html')
 
-app.run('127.0.0.1', '8081', debug=True)
+if __name__ == "__main__":
+    app.run('127.0.0.1', '8081', debug=True)
