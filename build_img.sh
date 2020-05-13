fc23a5d8fc1b82348e1d7502facddb3a49233a29
mosheb <mosheb3@gmail.com>
2020-05-14 00:05:52 +0300

    fix build_img.sh

--- build_img.sh
+++ build_img.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 if [ "$1" == "build" ];then
   docker build -t wog:latest .
+  docker build -t -f Dockerfile_web wogweb:latest .
 elif [ "$1" == "run" ];then
-  docker run -it --rm --name wog wog:latest python3 MainGame.py
+  docker run --rm -d -p 8081:8081 --name wogweb -v $(pwd):/app wogweb:latest
+  docker run -it --rm --name wog -v $(pwd):/app wog:latest
 else
   echo "Usage: bulid / run"
 fi
