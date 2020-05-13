50f357f5ef0d19e7289e5129e983884b30076bca
mosheb <mosheb3@gmail.com>
2020-05-13 20:46:58 +0300

    change build_img.sh

--- build_img.sh
+++ build_img.sh
@@ -2,7 +2,7 @@
 if [ "$1" == "build" ];then
   docker build -t wog:latest .
 elif [ "$1" == "run" ];then
-  docker run -it --name wog wog:latest python3 MainGame.py
+  docker run -it --rm --name wog wog:latest python3 MainGame.py
 else
   echo "Usage: bulid / run"
 fi
