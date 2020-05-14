909dc33c9f437d995ed07a63fc294da616727d65
mosheb <mosheb3@gmail.com>
2020-05-14 17:12:07 +0300

    commit from local copy

--- copyproj.sh
+++ copyproj.sh
@@ -5,9 +5,10 @@
 
 PROJ_NAME="WorldOfGames"
 
-scp /drives/d/PycharmProjects/${PROJ_NAME}/*.txt root@devops.local:/srv/projects/${PROJ_NAME}/
-scp /drives/d/PycharmProjects/${PROJ_NAME}/*.py root@devops.local:/srv/projects/${PROJ_NAME}/
-scp /drives/d/PycharmProjects/${PROJ_NAME}/Dockerfile root@devops.local:/srv/projects/${PROJ_NAME}/
-scp /drives/d/PycharmProjects/${PROJ_NAME}/Jenkinsfile root@devops.local:/srv/projects/${PROJ_NAME}/
+# scp /drives/d/PycharmProjects/${PROJ_NAME}/*.txt root@devops.local:/srv/projects/${PROJ_NAME}/
+#scp /drives/d/PycharmProjects/${PROJ_NAME}/*.py root@devops.local:/srv/projects/${PROJ_NAME}/
+#scp /drives/d/PycharmProjects/${PROJ_NAME}/Dockerfile root@devops.local:/srv/projects/${PROJ_NAME}/
+#scp /drives/d/PycharmProjects/${PROJ_NAME}/Jenkinsfile root@devops.local:/srv/projects/${PROJ_NAME}/
 
+rsync -ravz --exclude=.git/ --exclude=.idea/ --exclude=__pycache__ /drives/d/PycharmProjects/${PROJ_NAME}/* root@devops.local:/srv/projects/${PROJ_NAME}/
 ssh root@devops.local "cd /srv/projects/${PROJ_NAME}/ ; git commit -am 'commit from local copy'"
