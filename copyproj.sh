303ea7c53ee3ee495d367267d6f7b6ad463cc79f
mosheb <mosheb3@gmail.com>
2020-05-14 19:24:44 +0300

    commit from local copy

--- copyproj.sh
+++ copyproj.sh
@@ -10,5 +10,5 @@ PROJ_NAME="WorldOfGames"
 #scp /drives/d/PycharmProjects/${PROJ_NAME}/Dockerfile root@devops.local:/srv/projects/${PROJ_NAME}/
 #scp /drives/d/PycharmProjects/${PROJ_NAME}/Jenkinsfile root@devops.local:/srv/projects/${PROJ_NAME}/
 
-rsync -ravz --exclude=.git/ --exclude=.idea/ --exclude=__pycache__ /drives/d/PycharmProjects/${PROJ_NAME}/* root@devops.local:/srv/projects/${PROJ_NAME}/
+rsync -rv --exclude=.git/ --exclude=.idea/ --exclude=__pycache__ /drives/d/PycharmProjects/${PROJ_NAME}/* root@devops.local:/srv/projects/${PROJ_NAME}/
 ssh root@devops.local "cd /srv/projects/${PROJ_NAME}/ ; dos2unix *;git commit -am 'commit from local copy'"
