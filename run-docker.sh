331c9425a51a14ae5b8146f7bdd529b5774565d3
mosheb3 <mosheb3@gmail.com>
2020-05-14 18:40:51 +0300

    rename and change run-docker.sh

---
+++ run-docker.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+if [ "$1" == "build" ];then
+  docker build -t wog:latest .
+  docker build -f Dockerfile_web -t wog-web:latest .
+elif [ "$1" == "run" ];then
+  docker run --rm -d -p 8081:8081 --name wog-web -v $(pwd):/app wog-web:latest
+  docker run -it --rm --name wog -v $(pwd):/app wog:latest
+else
+  echo "Usage: run / bulid"
+fi
