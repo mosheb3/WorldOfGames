4652bc14d887f67117a4db60b573db464c17945d
mosheb3 <mosheb3@gmail.com>
2020-05-14 20:25:23 +0300

    fix LoadScore.py

--- LoadScore.py
+++ LoadScore.py
@@ -3,11 +3,15 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.keys import Keys
 
+def load_score():
+   chrome_options = webdriver.ChromeOptions()
+   chrome_options.add_argument("--incognito")
+   chrome_options.add_experimental_option("detach", True)
+   chrome_options.add_argument("--disable-extensions")
 
-chrome_options = webdriver.ChromeOptions()
-chrome_options.add_argument("--disable-extensions")
+   #driverCrm = webdriver.Chrome(executable_path='chromedriver/chromedriver.exe', chrome_options=chrome_options)
+   driverCrm = webdriver.Chrome(executable_path='chromedriver/chromedriver.exe', options=chrome_options)
 
-driverCrm = webdriver.Chrome(executable_path='chromedriver/chromedriver.exe', chrome_options=chrome_options)
-
-# Get into linked result of google search and try to login
-driverCrm.get("http://localhost:8081")
+   # Get into linked result of google search and try to login
+   # driverCrm.get("http://localhost:8081")
+   driverCrm.get("https://wog.local/")
