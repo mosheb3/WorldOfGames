f929b772772703fb60b334a7a752ac3927d15500
mosheb <mosheb3@gmail.com>
2020-05-17 22:56:31 +0300

    fix Jenkinsfile

--- runTests.sh
+++ runTests.sh
@@ -3,7 +3,7 @@ python3 MainGame.py < test_answers.txt
 
 grep "test123" data/data.json
 
-cat data/data.json | grep -i "test123"
+cat data/data.json | grep -i "test123333"
 
 if [ $? == 0 ]; then
   echo "Test Success"
