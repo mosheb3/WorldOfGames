5f53fd0a607a3881007652b1e73f7a0c995d4b2d
mosheb3 <mosheb3@gmail.com>
2020-05-17 15:51:44 +0300

    change run-docker.sh

--- run-docker.sh
+++ run-docker.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
 if [ "$1" == "build" ];then
-  docker build -t wog:latest .
-  docker build -f Dockerfile_web -t wog-web:latest .
+  if [ `/sbin/sysctl net.ipv4.conf.all.forwarding -n` != 1 ]; then
+    /sbin/sysctl -w net.ipv4.conf.all.forwarding=1
+    /sbin/sysctl -p
+  fi
+
+  docker build -t mosheb3/wog:latest .
+  docker build -f Dockerfile_web -t mosheb3/wog-web:latest .
 elif [ "$1" == "run" ];then
   docker stop wog-web
-  docker run --rm -d -p 8081:8081 --name wog-web -v $(pwd):/app wog-web:latest
-  docker run -it --rm --name wog -v $(pwd):/app wog:latest
+  docker run --rm -d -p 8081:8081 --name wog-web -v $(pwd):/app mosheb3/wog-web:latest
+  docker run -it --rm --name wog -v $(pwd):/app mosheb3/wog:latest
 else
   echo "Usage: run / bulid"
 fi
