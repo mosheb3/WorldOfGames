72dd9469f3da13e093c14b5c92613c05195ada07
mosheb3 <mosheb3@gmail.com>
2020-05-18 19:57:03 +0300

    change run-docker.sh

--- run-docker.sh
+++ run-docker.sh
@@ -1,17 +1,24 @@
 #!/bin/bash
 
+IMG_APP="mosheb3/wog"
+IMG_WEB="mosheb3/wog-web"
+IMG_APP_NAME="wog"
+IMG_WEB_NAME="wog-web"
+
 if [ "$1" == "build" ];then
    if [ `/sbin/sysctl net.ipv4.conf.all.forwarding -n` != 1 ]; then
       /sbin/sysctl -w net.ipv4.conf.all.forwarding=1
       /sbin/sysctl -p
    fi
 
-   docker build -t mosheb3/wog:latest .
-   docker build -f Dockerfile.web -t mosheb3/wog-web:latest .
+   docker build -t ${IMG_APP}:latest .
+   docker build -f Dockerfile.web -t ${IMG_WEB}:latest .
 elif [ "$1" == "run" ];then
-   docker stop wog-web
-   docker run --rm -d -p 8081:8081 --name wog-web -v $(pwd):/app mosheb3/wog-web:latest
-   docker run -it --rm --name wog -v $(pwd):/app mosheb3/wog:latest
+   if [ "$(docker inspect -f '{{.State.Running}}' ${IMG_WEB_NAME} 2>/dev/null)" != "true" ]; then
+      docker stop ${IMG_WEB_NAME}
+   fi
+      docker run --rm -d -p 8081:8081 --name ${IMG_WEB_NAME} -v $(pwd):/app ${IMG_WEB}:latest
+      docker run -it --rm --name ${IMG_APP_NAME} -v $(pwd):/app ${IMG_APP}:latest
 else
    echo "Usage: run / bulid"
 fi
