2494f97b32b85364d406eacf7ee3a6dc457aa8b3
mosheb <mosheb3@gmail.com>
2020-05-28 16:10:16 +0300

    add clean_build_push_run.sh

---
+++ clean_build_push_run.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+docker stop wog wog-web
+docker rm wog wog-web
+docker image rm mosheb3/wog-web mosheb3/wog
+
+./run-docker.sh build
+
+docker push mosheb3/wog:latest
+docker push mosheb3/wog-web:latest
+
+#docker-compose up
