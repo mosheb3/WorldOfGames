5eccfb84cf48ad0db1e2e02576f7f3969a18afc9
mosheb3 <mosheb3@gmail.com>
2020-05-31 23:14:40 +0300

    fix runGame.sh

--- runGame.sh
+++ runGame.sh
@@ -4,7 +4,7 @@
 service_name="wog-web"
 docker_compose_dir="/usr/local/bin"
 
-if [ -z `docker-compose ps -q $service_name` ] || [ -z `docker ps -q --no-trunc | grep $(docker-compose ps -q $service_name)` ]; then
+if [ -z `$docker_compose_dir/docker-compose ps -q $service_name` ] || [ -z `docker ps -q --no-trunc | grep $($docker_compose_dir/docker-compose ps -q $service_name)` ]; then
    $docker_compose_dir/docker-compose stop && $docker_compose_dir/docker-compose rm -f
    $docker_compose_dir/docker-compose up -d wog-web
    $docker_compose_dir/docker-compose run wog && $docker_compose_dir/docker-compose rm -f
